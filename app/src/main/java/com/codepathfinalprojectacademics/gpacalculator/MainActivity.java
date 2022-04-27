c0ee61f624921b08e1c6e90f7267429882ed9315
btjandra15 <btjandra15@gmail.com>
2022-04-27 02:43:35 -0400

    Almost finished the base for the 3 fragments
    FrameLayout not displaying the fragments correctly

--- app/src/main/java/com/codepathfinalprojectacademics/gpacalculator/MainActivity.java
+++ app/src/main/java/com/codepathfinalprojectacademics/gpacalculator/MainActivity.java
@@ -1,14 +1,55 @@
 package com.codepathfinalprojectacademics.gpacalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.codepathfinalprojectacademics.gpacalculator.databinding.ActivityMainBinding;
+import com.codepathfinalprojectacademics.gpacalculator.fragments.Classgpa;
+import com.codepathfinalprojectacademics.gpacalculator.fragments.Home;
+import com.codepathfinalprojectacademics.gpacalculator.fragments.Profile;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Home());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.home:
+                    Toast.makeText(this, "Switched to Home Fragment", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new Home());
+                    break;
+                case R.id.classgpa:
+                    Toast.makeText(this, "Switched to ClassGPA Fragment", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new Classgpa());
+                    break;
+                case R.id.profile:
+                    Toast.makeText(this, "Switched to Profile Fragment", Toast.LENGTH_SHORT).show();
+                    replaceFragment(new Profile());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
